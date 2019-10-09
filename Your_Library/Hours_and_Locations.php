9843296b2a810308a4da2823d9be03b472d087e6
Alison Pierson <alisonepierson@outlook.com>
2019-10-09 12:39:09 -0400

    Hours and Locations Just HTML

---
+++ Your_Library/Hours_and_Locations.php
@@ -0,0 +1,242 @@
+<?php
+include '../header.html';
+?>
+<div style="width:1204px; margin: auto;min-height: 75%;"">
+    <div class="">
+        <div id="col-sm-4">
+            <h1>Hours and Locations</h1>
+            <!--<ul>style="float: left; width: 33%;padding-left: 0px;margin-top: 3em;padding-right: 50px;display: block; "
+                <il><a href="#Victoria" class="sidebar">Victoria Avenue Branch</a></il>
+                <il><a href="#Community"class="sidebar">McBain Community Centre</a></il>
+                <il><a href="#Stamford"class="sidebar">Stamford Centre Library</a></il>
+                <il><a href="#Chippawa"class="sidebar">Chippawa</a></il>
+                <il><a href="#GaleCentre"class="sidebar">Gale Centre</a></il>
+            </ul>-->
+        </div>
+        <div id="Victoria">    
+            <h2>Victoria Avenue Branch</h2>
+        </div>
+        <div class="row">            
+            <div class="col-sm-5">
+                <h3>Hours</h3>
+                <Table >
+                    <tr>
+                        <th>Monday</th>
+                        <th>Tuesday</th>
+                        <th>Wednesday</th>
+                        <th>Thursday</th>
+                        <th>Friday</th>
+                        <th>Saturday</th>
+                        <th>Sunday</th>
+                    </tr>
+                    <tr>
+                        <td>9-8</td>
+                        <td>9-8</td>
+                        <td>9-8</td>
+                        <td>9-8</td>
+                        <td>9-5</td>
+                        <td>11-3</td>
+                        <td>11-3</td>
+                    </tr>
+                </Table>
+            </div>
+           <div class="col-sm-3">
+                <h3>Services available are;</h3>
+                <ul>
+                    <li>Bookable Rooms</li>
+                    <li>Free WiFi</li>
+                    <li>Public Computers</li>
+                    <li>Printing, Scanning & Copying</li>
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <h3>Address of this location</h3>
+                <p>4848 Victoria Avenue</br>
+                Niagara Falls, Ontario L2E 4C5</br>
+                Canada</p>  
+            </div>  
+        </div>
+        
+    </div>
+    <div id="Community" >
+        <h2>McBain Community Centre</h2>
+    </div>
+    <div class="row">
+        <div class="col-sm-5">
+            <h3>Hours</h3>
+            <Table>
+                <tr>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                </tr>
+                <tr>
+                    <td>9-8</td>
+                    <td>9-8</td>
+                    <td>9-8</td>
+                    <td>9-8</td>
+                    <td>9-8</td>
+                    <td>11-3</td>
+                    <td>11-3</td>
+                </tr>
+            </Table>
+        </div>
+         <div class="col-sm-3">
+                <h3>Services available are;</h3>
+                <ul>
+                    <li>Free WiFi</li>
+                    <li>Public Computers</li>
+                    <li>Printing, Scanning & Copying</li>
+                </ul>
+            </div>
+            <div class="col-sm-4">
+                <h3>Address of this location</h3>
+                <p>7150 Montrose Road</br>
+                Niagara Falls ON L2H 3N3</br>
+                Canada</p>  
+            </div>  
+       
+    </div>
+    <div id="Stamford">
+        <h2>Stamford Centre Library</h2>
+    </div>
+    <div class="row">
+        
+        <div class="col-sm-5">
+            <h3>Hours</h3>
+            <Table>
+                <tr>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                </tr>
+                <tr>
+                    <td>9-8</td>
+                    <td>9-5</td>
+                    <td>9-5</td>
+                    <td>9-8</td>
+                    <td>9-5</td>
+                    <td>11-3</td>
+                    <td>Closed</td>
+                </tr>
+            </Table>
+        </div>
+        <div class="col-sm-3">
+                <h3>Services available are;</h3>
+                <ul>
+                    <li>Bookable Rooms</li>
+                    <li>Free WiFi</li>
+                    <li>Public Computers</li>
+                    <li>Printing, Scanning & Copying</li>
+                </ul>
+        </div>
+        <div class="col-sm-4">
+            <h3>Address of this location</h3>
+            <p>Town and Country Plaza</br>
+            3643 Portage Road</br>
+            Niagara Falls ON  L2J 2K8</br>
+            Canada</p>  
+        </div>
+    </div>
+    
+    <div id="Chippawa">
+        <h2>Chippawa Library</h2> 
+    </div>
+    <div class="row">
+        
+        <div class="col-sm-5">
+            <h3>Hours</h3>
+            <Table>
+                <tr>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                </tr>
+                <tr>
+                    <td>12-8</td>
+                    <td>Closed</td>
+                    <td>9-8</td>
+                    <td>Closed</td>
+                    <td>9-5</td>
+                    <td>11-3</td>
+                    <td>Closed</td>
+                </tr>
+            </Table>
+        </div>
+        <div class="col-sm-3">
+            <h3>Services available are;</h3>
+            <ul>
+                <li>Bookable Rooms</li>
+                <li>Free WiFi</li>
+                <li>Public Computers</li>
+                <li>Printing, Scanning & Copying</li>
+            </ul>
+        </div>
+        <div class="col-sm-4">
+            <h3>Address of this location</h3>
+            <p>3763 Main Street</br>
+            Niagara Falls ON L2G 6B3</br>
+            Canada</p>  
+        </div> 
+    </div>
+    
+    <div id="GaleCentre">
+        <h2>NovelBranch at GaleCentre</h2> 
+    </div>
+    <div class="row">
+        <!--<iframe width="560" height="315" src="https://www.youtube.com/embed/0cxKjah3Zqc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>-->
+
+        <div class="col-sm-5">
+            <h3>Hours</h3>
+            <Table>
+                <tr>
+                    <th>Monday</th>
+                    <th>Tuesday</th>
+                    <th>Wednesday</th>
+                    <th>Thursday</th>
+                    <th>Friday</th>
+                    <th>Saturday</th>
+                    <th>Sunday</th>
+                </tr>
+                <tr>
+                <td>9-8</td>
+                <td>9-5</td>
+                <td>9-5</td>
+                <td>9-8</td>
+                <td>9-5</td>
+                <td>11-3</td>
+                <td>Closed</td>
+                </tr>
+            </Table>
+        </div>
+        <div class="col-sm-3">
+            <h3>Services available are;</h3>
+            <ul>
+                <li>Book Drop off</li>
+                <li>Borrow Magazines</li>
+            </ul>
+        </div>
+        <div class="col-sm-4">
+            <h3>Address of this location</h3>
+            <p>5152 Thorold Stone Rd,</br>
+            Niagara Falls, ON L2E 0A2</br>
+            Canada</p>  
+        </div> 
+    </div>
+    
+</div>
+<?php
+include '../footer.html';
+?>
